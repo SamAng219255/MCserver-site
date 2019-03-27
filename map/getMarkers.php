dae7962af1681d2794f8b55ac8310b23f93678a4
SamAng219255 <SamAng219255@gmail.com>
2019-03-27 13:48:44 -0700

    fixed bug in map/getMarkers.php

--- map/getMarkers.php
+++ map/getMarkers.php
@@ -36,7 +36,8 @@ if($queryresult) {for($i=0; $i<$queryresult->num_rows; $i++) {
 	if($i>0) {
 		echo ',';
 	}
-	echo '{"id":'.$row[0].',"user":"'.$row[1].'","name":"'.str_replace('"','\\"',$row[2]).'","desc":"'.str_replace('"','\\"',$row[3]).'","x":'.$row[4].',"z":'.$row[5].',"dimension":'.$row[6].'}';
+	echo '{"id":'.$row[0].',"user":"'.$row[1].'","name":"'.str_replace('"','\\"',$row[2]).'","desc":"'.str_replace("	","\\t",htmlspecialchars(str_replace(array("\r\n","\r","\n",'
+'), "\\n", $row[3]))).'","x":'.$row[4].',"z":'.$row[5].',"dimension":'.$row[6].'}';
 }}
 
 echo ']';
