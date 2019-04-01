8717d7699a549184ef62eafa85ed17cdad5701a2
SamAng219255 <SamAng219255@gmail.com>
2019-03-31 17:56:12 -0700

    some minor fixes

--- getServerStatus.php
+++ getServerStatus.php
@@ -3,7 +3,7 @@ session_start();
 
 if($_SESSION['permissions']>=1) {
 	$response=shell_exec("./getServerStatus");
-	echo '{"on":'.($response>0 ? 'true' : 'false').',"allowed":true,"response":"'.json_encode($response).'"}';
+	echo '{"on":'.($response>0 ? 'true' : 'false').',"allowed":true,"response":'.json_encode($response).'}';
 }
 else {
 	echo '{"on":false,"allowed":false}';
--- turnOnServer.php
+++ turnOnServer.php
@@ -3,7 +3,7 @@ session_start();
 
 if($_SESSION['permissions']>=1) {
 	$response=shell_exec("./turnOnServer");
-	echo '{"allowed":true,"response":"'.json_encode($response).'"}';
+	echo '{"allowed":true,"response":'.json_encode($response).'}';
 }
 else {
 	echo '{"allowed":false}';
