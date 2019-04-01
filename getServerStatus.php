b1752d6921a1a4c939c9b9508a712ab7a4a59584
SamAng219255 <SamAng219255@gmail.com>
2019-03-31 17:48:04 -0700

    some minor fixes

--- getServerStatus.php
+++ getServerStatus.php
@@ -2,10 +2,10 @@
 session_start();
 
 if($_SESSION['permissions']>=1) {
-	echo '{"on":'.(shell_exec("./getServerStatus")>0 ? 'true' : 'false').'}';
+	echo '{"on":'.(shell_exec("./getServerStatus")>0 ? 'true' : 'false').',"allowed":true}';
 }
 else {
-	echo '{"on":false}';
+	echo '{"on":false,"allowed":false}';
 }
 
 ?>
